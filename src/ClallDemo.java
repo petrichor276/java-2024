092f7edcd7bb436d1246b82a60c8757628a89f35
petrichor276 <155598405+petrichor276@users.noreply.github.com>
2024-01-08 16:16:12 +0800

    Changes

---
+++ src/ClallDemo.java
@@ -0,0 +1,24 @@
+public class ClallDemo {
+    //成员变量
+    String name;
+    int age;
+    //成员方法
+    public void call() {
+        System.out.println(name + "正在打电话");
+    }
+    public void message() {
+        System.out.println(name + "今年" + age + "岁了");
+    }
+
+    public static void main(String[] args) {
+        ClallDemo t = new ClallDemo();
+        // 调用成员变量
+        t.name = "小明";
+        t.age = 18;
+        // 调用成员方法
+        t.call();
+        t.message();
+        // 若不需要对象名，也可以直接使用对象
+        new ClallDemo().message();
+    }
+}
